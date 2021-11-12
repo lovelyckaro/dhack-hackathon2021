35f2b58f95cf720519ccfd9f7a39101d8bc4f1a0
Brage Salhus Bunk <bragesb@gmail.com>
2021-11-12 16:33:30 +0100

    Working POST endpoint with IO effects

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -4,8 +4,23 @@ module Main where
 
 import Web.Scotty
 
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+import Control.Monad.IO.Class (MonadIO(liftIO))
+
 main :: IO ()
 main = do
-  scotty 3000 $
-    get "/" $
-      html "<h1>Hello World!</h1>"
+  scotty 3000 $ do
+    get "/" $ do
+      html $ renderHtml $ 
+        H.html $
+          H.body $ do
+            H.h1 "Do a thing!"
+            H.form H.! A.method "post" H.! A.action "/" $ do
+              H.input H.! A.type_ "submit"
+    post "/" $ do
+      liftIO $ putStrLn "A thing!"
+      redirect "/"
